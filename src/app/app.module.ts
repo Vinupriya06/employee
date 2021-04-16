47c90d9393f5f53473d734aa900420c92d8b1aac
Vinupriya06 <vinugeetha21@gmail.com>
2021-04-16 22:15:44 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { ReportComponent } from './report/report.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    ReportComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
